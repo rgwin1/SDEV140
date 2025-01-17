2c04c9d6843bc60dbb7c765dc46814e96a2a53af
Ryan Gwin <rgwin1@ivytech.edu>
2025-01-17 15:58:14 -0500

    added part1 (celsius converter) and part2 (restaurant bill calculator) python files

---
+++ Module1/GwinRyanM1E1.py
@@ -0,0 +1,29 @@
+"""
+
+Author: Ryan Gwin
+Date written: 01/17/2025
+Assignment:   Module1 Programming Assignment Part1
+Short Desc:   take temperature in celsius as user input, convert temp to Fahrenheit, 
+            print result to screen
+
+
+"""
+
+
+
+def celsiusToFahrenheit():
+    userTemp = input("Enter the temperature in celsius you want converted to Fahrenheit: ")
+    conversion  = ((9/5)*float(userTemp))+32
+    print(conversion)
+    """
+        This function takes user's input, degrees celsius and converts it to fahrenheit,
+        and prints to screen
+
+    """
+
+celsiusToFahrenheit()
+
+
+
+
+ 
---
+++ Module1/GwinRyanM1E2.py
@@ -0,0 +1,35 @@
+"""
+
+Author: Ryan Gwin
+Date written: 01/17/2025
+Assignment:   Module1 Programming Assignment Part2
+Short Desc:  get restaurant bill pre-tax, pre-tip as user input, 
+            calculate tax with rate of 7%, 
+            calculate tip at 18%,
+            add to bill for total bill
+            print to screen
+
+
+"""
+
+
+
+def billCalculator():
+    """ 
+    This function takes user input, bill, calculates tax, calculates tip, 
+    adds them together and prints to screen.
+        
+    """
+    tipPercentage = 0.18
+    taxRate = 0.07
+    bill = input("What is the cost of the bill: ")
+    tax = float(bill) * taxRate
+    billWithTax = float(bill) + tax
+    tip = billWithTax * tipPercentage
+    total = billWithTax + tip
+    print(f"Your total is ${total}.")
+    
+
+    
+
+billCalculator()
