07fb6b43f06368faeeffe49c89f1cc100285c123
Ryan <rgwin1@ivytech.edu>
2025-02-03 16:36:02 -0500

    prepared file for submission

--- Module3/GwinRyanM3PA-2.py
+++ Module3/GwinRyanM3PA-2.py
@@ -1,14 +1,16 @@
 """
 Author: Ryan Gwin
 Date Written: 02/01/2025
+Assignment: Module3 Programming Assignment Part 2
 
 Instructions: 
-Write a program that writes a series of random numbers to a file and displays all the numbers to the console. Each random number should be in the range of 1 through 500. The application should let the user specify how many random numbers the file will hold. (15 points)
+Write a program that writes a series of random numbers to a file and displays all the numbers to the console. 
+Each random number should be in the range of 1 through 500. 
+The application should let the user specify how many random numbers the file will hold. (15 points)
 
 """
 #used for random value generation
 import random
-
 
 #get user input to determine how many numbers will be written to the file and printed to the console
 totalNums = input("Please enter the total number of random integers you want to write to the file: ")
@@ -21,10 +23,11 @@ totalNums = int(totalNums)
 #generate array of random numbers. 
 randomNumbers = [random.randint(0, 500) for randomNumber in range(1, totalNums + 1)]
 # print(randomNumbers)
-with open("randomNumbers.txt", "w") as file:
+with open("randomNumbers.txt", "a") as file:
+    file.write(f'new array:\n')
     for number in randomNumbers:
         if (number == randomNumbers[-1]):
-            file.write(str(number))
+            file.write(str(number) + "\n")
         else:
             file.write(str(number) + ', ')
 file.close()
